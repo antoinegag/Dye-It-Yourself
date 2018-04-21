335265cb5b1c21f19d57ef5a068c7d5d4b2928d8
Poke <antoine.gagnon760@hotmail.com>
2018-04-21 14:25:10 -0400

    Bumb

--- src/main/java/xyz/poketech/diy/DyeItYourself.java
+++ src/main/java/xyz/poketech/diy/DyeItYourself.java
@@ -17,7 +17,7 @@ public class DyeItYourself {
     public static final String MODID = "diy";
 
     public static final String NAME = "Dye it yourself";
-    public static final String VERSION = "1.1";
+    public static final String VERSION = "1.1.1";
     public static final String VERSION_RANGE = "[1.12,1.13)";
     public static final boolean DEV_MODE = (Boolean) Launch.blackboard.get("fml.deobfuscatedEnvironment");
 
