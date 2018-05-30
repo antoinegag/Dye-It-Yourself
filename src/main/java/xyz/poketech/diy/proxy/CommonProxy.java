0b55e1dd2ab636e61217590c0975370a5c8e86d0
Poke <antoine.gagnon760@hotmail.com>
2018-05-29 20:54:23 -0400

    Add compat for morph
    Avoid each sheep  being seen as unique and causing duplicate

--- src/main/java/xyz/poketech/diy/proxy/CommonProxy.java
+++ src/main/java/xyz/poketech/diy/proxy/CommonProxy.java
@@ -4,6 +4,7 @@ import net.minecraft.block.Block;
 import net.minecraft.item.Item;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLInterModComms;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
@@ -17,6 +18,9 @@ public class CommonProxy {
 
     public void preInit(FMLPreInitializationEvent e) {
         PacketHandler.registerMessages();
+
+        FMLInterModComms.sendMessage("morph", "nbt_modifier", "net.minecraft.entity.passive.EntitySheep>nextDye>13370");
+        FMLInterModComms.sendMessage("morph", "nbt_modifier", "net.minecraft.entity.passive.EntitySheep>diy_color>0");
     }
 
     public void init(FMLInitializationEvent e) {
